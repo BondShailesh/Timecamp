8f15042653c5623932a9fe6937f3d2ffe3a65cff
Ashish <ashisprusty71@gmail.com>
2022-08-27 22:56:46 +0530

    adding user project list in dasboard

--- Frontend/src/routes/Allroute.jsx
+++ Frontend/src/routes/Allroute.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Project from "../pages/Project";
 import Signup from "../pages/Signup";
+import User from "../pages/User";
 import BookDemo from "../Sauravcomp/BookDemo";
 import Main from "../Sauravcomp/Main";
 import MainContent from "../Sauravcomp/MainContent";
+import Task from "../Sauravcomp/Task";
 
 
 
@@ -18,6 +21,10 @@ const Allroute = () => {
        <Route path ="/" element = {<Home/>} ></Route>
        <Route path= "/app" element ={<Main/>}></Route>
        <Route path= "/bookdemo" element ={<BookDemo/>}></Route>
+       <Route path="/app/user" element = {<User/>} ></Route>
+       <Route path="/app/task" element = {<Task/>} ></Route>
+       <Route path="/app/project" element = {<Project/>} ></Route>
+
       </Routes>
     </div>
   );
